030ccf242a2d0da06cbbf6aefbb9dae99aa27233
Igor Kim <igor.skh@gmail.com>
2019-08-17 00:17:45 +0200

    initial configuration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Container, Row, Col } from "react-bootstrap";
+
+class App extends React.Component {
+  render() {
+    return <Container>
+      <Row>
+        <Col></Col>
+        <Col lg={6} md={6} sm={12}>
+          
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>;
+  }
 }
 
 export default App;
