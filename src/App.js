f0f577ff34640cfe7cceea7c937292bad691092b
Igor Kim <igor.skh@gmail.com>
2019-08-18 12:22:44 +0200

    update app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from "react";
-import "./App.css";
-
 import { Container, Row, Col } from "react-bootstrap";
 
+import AddProductForm from "./Components/AddProductForm";
+import ProductList from "./Components/ProductList";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "./Styles/App.css";
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.productListChild = React.createRef();
+  }
+
   render() {
     return <Container>
       <Row>
         <Col></Col>
-        <Col lg={6} md={6} sm={12}>
-          
+        <Col lg={6} md={8} sm={12}>
+          <h2>Smart Shopping List</h2>
+          <AddProductForm
+            triggerItemAdd={(item) => this.productListChild.current.onItemAdd(item)} />
+          <ProductList
+            ref={this.productListChild} />
         </Col>
         <Col></Col>
       </Row>
